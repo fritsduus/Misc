5fabbb8f4001b9ba985f20900da28ff0b4826c97
Frits Duus Jørgensen <frits.duus@gmail.com>
2014-08-31 22:11:10 +0200

    Added comment with error text to failing test

--- MiscTest/SerilogTest.cs
+++ MiscTest/SerilogTest.cs
@@ -16,6 +16,7 @@ namespace MiscTest
             Assert.AreEqual(1, 1);
         }
 
+        // Fails: with the error: An exception occurred while invoking executor 'executor://mstestadapter/v1': Type is not resolved for member 'Serilog.Context.ImmutableStack`1[[Serilog.Core.ILogEventEnricher, Serilog, Version=1.4.0.0, Culture=neutral, PublicKeyToken=24c2f752a8e58a10]],Serilog.FullNetFx, Version=1.4.0.0, Culture=neutral, PublicKeyToken=24c2f752a8e58a10'.
         [TestMethod]
         public void TestPushProperty()
         {
